15e430f1d00e98b9e6c4db81d0c0e97b2dbdcadf
Joe Wallwork <jw2423@cam.ac.uk>
2026-09-09 11:15:16 +0100

    Apply Fortitude

--- src/mod_namelist.f90
+++ src/mod_namelist.f90
@@ -17,19 +17,19 @@ module mesc_namelist
     integer :: jrestart = 0
     integer :: jmodel = 1
     integer :: ifsoc14 = 0
-    character(len=path_len) :: frestart_in = ''
-    character(len=path_len) :: frestart_out = ''
-    character(len=path_len) :: foutput = ''
-    character(len=path_len) :: fparameter = ''
-    character(len=path_len) :: cfraction = ''
-    character(len=path_len) :: frac14c = ''
-    character(len=path_len) :: f14c(5) = ''
-    character(len=path_len) :: filecluster = ''
-    character(len=path_len) :: fhwsdsoc = ''
-    character(len=path_len) :: faustsoc = ''
-    character(len=path_len) :: fmodis = ''
-    character(len=path_len) :: fanoc = ''
-    character(len=path_len) :: fglobal(7) = ''
+    character(len=path_len) :: frestart_in = ""
+    character(len=path_len) :: frestart_out = ""
+    character(len=path_len) :: foutput = ""
+    character(len=path_len) :: fparameter = ""
+    character(len=path_len) :: cfraction = ""
+    character(len=path_len) :: frac14c = ""
+    character(len=path_len) :: f14c(5) = ""
+    character(len=path_len) :: filecluster = ""
+    character(len=path_len) :: fhwsdsoc = ""
+    character(len=path_len) :: faustsoc = ""
+    character(len=path_len) :: fmodis = ""
+    character(len=path_len) :: fanoc = ""
+    character(len=path_len) :: fglobal(7) = ""
     real(dp) :: xopt(16) = 1.0_dp
     integer :: nxopt(16) = [1, 2, 3, 4, 5, 6, 7, 8, &
                             9, 10, 11, 12, 13, 14, 15, 16]
@@ -90,8 +90,8 @@ contains
     xopt = config%xopt
     nxopt = config%nxopt
 
-    open(newunit=nml_unit, file=trim(filename), status='old', &
-         action='read', iostat=ios, iomsg=iomsg)
+    open(newunit=nml_unit, file=trim(filename), status="old", &
+         action="read", iostat=ios, iomsg=iomsg)
     if (ios /= 0) call fatal_error('Cannot open "' // trim(filename) // '": ' // trim(iomsg))
 
     read(nml_unit, nml=mesc, iostat=ios, iomsg=iomsg)
@@ -129,24 +129,24 @@ contains
   subroutine print_mesc_config(config)
     type(mesc_config), intent(in) :: config
       !! Derived type instance holding parameter values
-    write(output_unit, '(a,i0)') 'runcase     = ', config%runcase
-    write(output_unit, '(a,l1)') 'jglobal     = ', config%jglobal
-    write(output_unit, '(a,i0)') 'kinetics    = ', config%kinetics
-    write(output_unit, '(a,i0)') 'bgcopt      = ', config%bgcopt
-    write(output_unit, '(a,i0)') 'jopt        = ', config%jopt
-    write(output_unit, '(a,i0)') 'jrestart    = ', config%jrestart
-    write(output_unit, '(a,i0)') 'jmodel      = ', config%jmodel
-    write(output_unit, '(a,i0)') 'ifsoc14     = ', config%ifsoc14
-    write(output_unit, '(a,a)')  'foutput     = ', trim(config%foutput)
-    write(output_unit, '(a,a)')  'fparameter  = ', trim(config%fparameter)
-    write(output_unit, '(a,a)')  'fhwsdsoc    = ', trim(config%fhwsdsoc)
-    write(output_unit, '(a,a)')  'fanoc       = ', trim(config%fanoc)
+    write(output_unit, "(a,i0)") "runcase     = ", config%runcase
+    write(output_unit, "(a,l1)") "jglobal     = ", config%jglobal
+    write(output_unit, "(a,i0)") "kinetics    = ", config%kinetics
+    write(output_unit, "(a,i0)") "bgcopt      = ", config%bgcopt
+    write(output_unit, "(a,i0)") "jopt        = ", config%jopt
+    write(output_unit, "(a,i0)") "jrestart    = ", config%jrestart
+    write(output_unit, "(a,i0)") "jmodel      = ", config%jmodel
+    write(output_unit, "(a,i0)") "ifsoc14     = ", config%ifsoc14
+    write(output_unit, "(a,a)")  "foutput     = ", trim(config%foutput)
+    write(output_unit, "(a,a)")  "fparameter  = ", trim(config%fparameter)
+    write(output_unit, "(a,a)")  "fhwsdsoc    = ", trim(config%fhwsdsoc)
+    write(output_unit, "(a,a)")  "fanoc       = ", trim(config%fanoc)
   end subroutine print_mesc_config
 
   !> Raise a fatal error, reporting a given message.
   subroutine fatal_error(message)
     character(len=*), intent(in) :: message  !! Message to report
-    write(error_unit, '(a)') 'ERROR: ' // trim(message)
+    write(error_unit, "(a)") "ERROR: " // trim(message)
     error stop 1
   end subroutine fatal_error
 
